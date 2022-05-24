d8534d75b78ecc9ead30b1a184dfe20561eba1b3
Ingrid Queiroz <queirozingrid.c@gmail.com>
2022-05-24 13:41:14 -0300

    adicionando cors config

--- src/main/java/com/squirtle/weekend/controller/CategoriaController.java
+++ src/main/java/com/squirtle/weekend/controller/CategoriaController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaController {
---
+++ src/main/java/com/squirtle/weekend/security/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.squirtle.weekend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}

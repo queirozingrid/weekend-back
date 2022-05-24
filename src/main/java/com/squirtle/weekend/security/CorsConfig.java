df50bd8dc3bf62065511b469d8b9da93fbe1ae8e
Ingrid Queiroz <queirozingrid.c@gmail.com>
2022-05-24 14:19:30 -0300

    alterando metodos

--- src/main/java/com/squirtle/weekend/security/CorsConfig.java
+++ src/main/java/com/squirtle/weekend/security/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS")
                         .allowedOrigins("*")
                         .allowedHeaders("*");
             }

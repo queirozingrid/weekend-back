c53f0dd619ff7cb6927dc11b2f76e914d781a113
Ingrid Queiroz <queirozingrid.c@gmail.com>
2022-06-21 17:20:28 -0300

    testando teoria da conspiração

--- src/main/java/com/squirtle/weekend/filesManager/FileSaver.java
+++ src/main/java/com/squirtle/weekend/filesManager/FileSaver.java
@@ -13,19 +13,19 @@ public class FileSaver {
     private static Storage storage = StorageOptions.getDefaultInstance().getService();
 
     public static String saveLogo(MultipartFile logo){
-        try {
-            BlobInfo blob = storage.create(
-                    BlobInfo.newBuilder("imagens-wknd", logo.getOriginalFilename()).build(),
-                    logo.getBytes(),
-                    Storage.BlobTargetOption.predefinedAcl(Storage.PredefinedAcl.PUBLIC_READ)
-            );
-            System.out.printf(blob.getMediaLink());
-            return blob.getMediaLink();
-
-        } catch (IllegalStateException | IOException e){
-            throw new RuntimeException(e);
-        }
-
+//        try {
+//            BlobInfo blob = storage.create(
+//                    BlobInfo.newBuilder("imagens-wknd", logo.getOriginalFilename()).build(),
+//                    logo.getBytes(),
+//                    Storage.BlobTargetOption.predefinedAcl(Storage.PredefinedAcl.PUBLIC_READ)
+//            );
+//            System.out.printf(blob.getMediaLink());
+//            return blob.getMediaLink();
+//
+//        } catch (IllegalStateException | IOException e){
+//            throw new RuntimeException(e);
+//        }
+        return null;
     }
 
     public FileSaver(String path) {
